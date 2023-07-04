ebb4f4aec25d91c920e939c38b811e0859f4251f
Eyiiiiii <133078366+Eyiiiiii@users.noreply.github.com>
2023-07-04 20:06:06 -0200

    used, form action attribute to redirect next page
    uses pattern required field to only accept default (TUPM-00-0000)

--- js/login.js
+++
@@ -1,7 +0,0 @@
-function handleGoogleButtonClick() {
-  window.location.href = "https://accounts.google.com/";
-}
-
-function handleMicrosoftButtonClick() {
-  window.location.href = "https://login.live.com/";
-}
